9675acda904c8d9aab874f4e10b0a8a27ff0b32c
Rayane <45128167+YiraSan@users.noreply.github.com>
2021-12-30 23:35:44 +0100

    build

---
+++ lib/lexer/types.d.ts
@@ -0,0 +1,8 @@
+import { Long, Token as _Token, Unique, Wrapper } from "rale";
+export declare const defaultWrapper: Wrapper[];
+export declare const defaultLong: Long[];
+export declare const defaultUnique: Unique[];
+export declare type TokenName = "curly_bracket" | "bracket" | "parenthesis" | "angle_bracket" | "number" | "string" | "dot" | "semicolon" | "colon" | "dash" | "plus" | "equal" | "star" | "slash" | "anti_slash" | "question_mark" | "exclamation_mark" | "double_quote" | "single_quote" | "backtick";
+export interface Token extends _Token {
+    name: TokenName;
+}
